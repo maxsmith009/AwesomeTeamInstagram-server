998f376f432b5ab167b9fc46336f9f7ac53f4c66
Maksim Zalutski <max03246@gmail.com>
2019-06-04 09:48:04 +0300

    Bootstrap app services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './controllers/posts/PostsController';
 import { PostsService } from './services/posts/PostsService';
 import { AuthModule } from './auth/auth.module';
+import { CommentsService } from './services/comments/CommentsService';
+import { ProfilesService } from './services/profiles/ProfilesService';
+import { EmailService } from './services/emails/EmailService';
 
 
 @Module({
-  imports: [AuthModule],
-  controllers: [PostsController],
-  providers: [PostsService],
+  imports: [
+    AuthModule,
+  ],
+  controllers: [
+    PostsController,
+  ],
+  providers: [
+    PostsService,
+    CommentsService,
+    ProfilesService,
+    EmailService],
 })
-export class AppModule {}
+export class AppModule {
+}
